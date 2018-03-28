1a788473deeab6416aa2a1822294425285ef0f97
jljardon <github email address>
2018-03-28 01:59:29 -0400

    add create user validations
    Added validations for email and username. Added flash message for their respective cases.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
-   get '/users/signup' do
-      logged_in? ? (redirect '/posts') : (erb :'users/create_user')
-   end
+  use Rack::Flash
+  get '/users/signup' do
+    logged_in? ? (redirect '/posts') : (erb :'users/create_user')
+  end
 
-   post '/users/signup' do
-     # binding.pry
-      if params[:username].empty? || params[:email].empty? || params[:password].empty?
-         redirect '/signup'
-      else
-         session[:user_id] = User.create(username: params[:username], password: params[:password], email: params[:email]).id
-         redirect '/posts'
-      end
-   end
+  post '/users/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      flash[:message] = 'Username, password and email fields cannot be empty!'
+      redirect '/users/signup'
+    elsif User.find_by(email: params[:email])
+      flash[:message] = 'Email already exists in our records. Please <a href="/">login</a> or user another email.'
+      redirect '/users/signup'
+    elsif User.find_by(username: params[:username])
+      flash[:message] = 'Username already registered, please choose another one.'
+      redirect '/users/signup'
+    else
+      session[:user_id] = User.create(username: params[:username], password: params[:password], email: params[:email]).id
+      redirect '/posts'
+    end
+  end
 end
