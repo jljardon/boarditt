f22291c926628ab9a4dc61dcd08be8dfd97a8073
jljardon <github email address>
2018-03-29 01:51:37 -0400

    add create post functionality
    added functionality to create posts and validations to preserve all relevant data of that post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
+  # use Rack::Flash
   get '/posts' do
     logged_in? ? (erb :'posts/posts') : (redirect '/')
+  end
+
+  get '/posts/new' do
+    logged_in? ? (erb :'posts/new_post') : (redirect '/')
+  end
+
+  post '/posts/new' do
+    if params[:title].empty? || params[:content].empty?
+      flash[:message] = 'Title and content fields cannot be empty!'
+      erb :'/posts/new_post'
+    else
+      @post = Post.create(title: params[:title], content: params[:content])
+      @post.user = current_user
+      @post.save
+      redirect "/posts/#{@post.id}/#{@post.slug}"
+    end
   end
 
   get '/posts/:id/:slug' do
