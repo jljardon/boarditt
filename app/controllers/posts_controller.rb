e1dabcc0c1c39edcd0fc08459c0217bc83b67a62
jljardon <github email address>
2018-03-30 00:29:48 -0400

    add edit post action
    added edit post action and form with validations of user, session and content.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   post '/posts/new' do
     if params[:title].empty? || params[:content].empty?
       flash[:message] = 'Title and content fields cannot be empty!'
-      erb :'/posts/new_post'
+      erb :'posts/new_post'
     else
       @post = Post.create(title: params[:title], content: params[:content])
       @post.user = current_user
@@ -21,8 +21,28 @@ class PostsController < ApplicationController
 
   delete '/posts/:id/delete' do
     @post = Post.find_by(id: params[:id])
-    @post.destroy
-    redirect '/posts'
+    if @post.user.id = current_user.id
+      @post.destroy
+      redirect '/posts'
+    else
+      redirect "/posts/#{@post.id}/#{@post.slug}"
+    end
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+    erb :'posts/edit_post'
+  end
+
+  patch '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+    if @post.user.id = current_user.id && !params[:title].empty? || !params[:content].empty?
+      @post.title = params[:title]
+      @post.content = params[:content]
+      @post.save
+    end
+
+    redirect "/posts/#{@post.id}/#{@post.slug}"
   end
 
   get '/posts/:id/:slug' do
