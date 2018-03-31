d24f41df9d8a9544c21878e0afa7d25ba4170944
jljardon <github email address>
2018-03-31 16:45:29 -0400

    add edit comment action
    Added functionality to edit comments in the user view and post view.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   use Rack::Flash
   get '/comments/new/:post_id' do
     @post_id = params[:post_id]
-    erb :'comments/new_comment'
+    logged_in? ? (erb :'comments/new_comment') : (redirect '/')
   end
 
   post '/comments/new/:post_id' do
@@ -17,6 +17,22 @@ class CommentsController < ApplicationController
     if @comment.user.id == @user.id
       @comment.destroy
       flash[:message] = 'Your comment was deleted.'
+    end
+    erb :'users/show_user'
+  end
+
+  get '/comments/:id/edit' do
+    @comment = Comment.find_by(id: params[:id])
+    logged_in? ? (erb :'comments/edit_comment') : (redirect '/')
+  end
+
+  patch '/comments/:id/edit' do
+    @comment = Comment.find_by(id: params[:id])
+    @user = current_user
+    if @comment.user.id == @user.id
+      @comment.content = params[:content]
+      @comment.save
+      flash[:message] = 'Your comment was edited successfully.'
     end
     erb :'users/show_user'
   end
